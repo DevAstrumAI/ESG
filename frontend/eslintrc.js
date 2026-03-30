74d3d7bbcc797810822052b2608666a12e84e02b
noorulain212 <ainnyy2428@gmail.com>
2026-03-30 13:10:26 +0500

    updated package.json

---
+++ frontend/eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+    extends: ['react-app'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  };
